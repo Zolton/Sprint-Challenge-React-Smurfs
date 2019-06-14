c220080f9078925df2c09629422bde437c4d487c
Zolton <gmzesq@gmail.com>
2019-06-14 11:54:38 -0400

    Finished stage 1 of MVP

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,49 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+//import { Router } from "react-router-dom";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import axios from "axios"
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+  componentDidMount () {
+    axios
+    .get("http://localhost:3333/smurfs")
+    // .then(res=>console.log(res))
+    // .then(res=>console.log(res.data))
+    .then(res=>this.setState({smurfs: res.data}))
+  }
+
+  addSmurf = newSmurf => {
+    axios
+    .post("http://localhost:3333/smurfs", newSmurf)
+    .then(res=>this.setState({smurfs: res.data}))
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    console.log("this is smurfs")
+    console.log(this.state.smurfs)
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+       
+          <SmurfForm addSmurf={this.addSmurf} /> 
+            
+          
+          
+            <Smurfs smurfs={this.state.smurfs} />
+          
+        
       </div>
     );
   }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
 class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+ 
+    state = {
+      newSmurf: {
       name: '',
       age: '',
       height: ''
+      }
     };
-  }
+  
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    this.props.addSmurf(this.state.newSmurf)
     this.setState({
       name: '',
       age: '',
@@ -22,7 +23,12 @@ class SmurfForm extends Component {
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ 
+      newSmurf: {
+        ...this.state.newSmurf,
+      [e.target.name]: e.target.value 
+      }
+    });
   };
 
   render() {
@@ -32,19 +38,19 @@ class SmurfForm extends Component {
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.newSmurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.newSmurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.newSmurf.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
